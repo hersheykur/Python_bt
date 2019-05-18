7aa1df2100233b72912e46829e12e6cc880f46b2
hersheykur <50768777+hersheykur@users.noreply.github.com>
2019-05-18 16:02:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, redirect, url_for, render_template, request, flash
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import braintree
+from gateway import generate_client_token, transact, find_transaction
+
+app = Flask(__name__)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+app.secret_key = os.environ.get('APP_SECRET_KEY')
+
+PORT = int(os.environ.get('PORT', 3432))
+
+TRANSACTION_SUCCESS_STATUSES = [
+    braintree.Transaction.Status.Authorized,
+    braintree.Transaction.Status.Authorizing,
+    braintree.Transaction.Status.Settled,
+    braintree.Transaction.Status.SettlementConfirmed,
+    braintree.Transaction.Status.SettlementPending,
+    braintree.Transaction.Status.Settling,
+    braintree.Transaction.Status.SubmittedForSettlement
+]
+
+@app.route('/', methods=['GET'])
+def index():
+    return redirect(url_for('new_checkout'))
+
+@app.route('/checkouts/new', methods=['GET'])
+def new_checkout():
+    client_token = generate_client_token()
+    return render_template('checkouts/new.html', client_token=client_token)
+
+@app.route('/checkouts/<transaction_id>', methods=['GET'])
+def show_checkout(transaction_id):
+    transaction = find_transaction(transaction_id)
+    result = {}
+    if transaction.status in TRANSACTION_SUCCESS_STATUSES:
+        result = {
+            'header': 'sweet success!',
+            'icon': 'Success',
+            'message': 'Your test transaction has been successfully processed. See the Braintree API response and try again.'
+        }
+    elif transaction.status not in TRANSACTION_SUCCESS_STATUSES:
+        result = {
+            'header': 'Transaction Failed',
+            'icon': 'fail',
+            'message': 'Your test transaction has failed with status' + transaction.status + '. See the Braintree API response and try again.'
+        }
+    else:
+        result = {
+            'header': 'Transaction refund',
+            'icon': 'refund',
+            'message': 'Your test transaction has a status of refund'
+        }
+
+    return render_template('checkouts/show.html', transaction=transaction, result=result)
+
+@app.route('/checkouts', methods=['POST'])
+def create_checkout():
+    result = transact({
+        'amount': request.form['amount'],
+        'payment_method_nonce': request.form['payment_method_nonce'],
+        'options': {
+            "submit_for_settlement": True
+        }
+    })
+
+    if result.is_success or result.transaction:
+        return redirect(url_for('show_checkout',transaction_id=result.transaction.id))
+    else:
+        for x in result.errors.deep_errors: flash('Error: %s: %s' % (x.code, x.message))
+        return redirect(url_for('new_checkout'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT, debug=True)
+    print(transaction_id)
